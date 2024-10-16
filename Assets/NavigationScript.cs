f904a6bf8feb59296c6d44ac7cbd237d22939afb
Julio Cesar <jc.urbano.vasconcelos@gmail.com>
2024-10-16 02:00:42 -0300

    Navigation with any input

--- Assets/NavigationScript.cs
+++ Assets/NavigationScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NavigationScript : MonoBehaviour
 {   
@@ -13,6 +14,8 @@ public class NavigationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown) {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 }
